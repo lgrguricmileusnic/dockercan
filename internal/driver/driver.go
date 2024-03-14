41c58d7944897416bb587388b0122c83c40af2ba
Lovro Grguric Mileusnic <lovrogm@gmail.com>
2024-03-15 00:02:20 +0100

    Implement GetCapabilities endpoint

--- internal/driver/driver.go
+++ internal/driver/driver.go
@@ -1,8 +1,6 @@
 package driver
 
 import (
-	"fmt"
-
 	"github.com/docker/go-plugins-helpers/network"
 )
 
@@ -11,57 +9,47 @@ type Driver struct {
 }
 
 func (d *Driver) GetCapabilities() (*network.CapabilitiesResponse, error) {
-	return nil, nil
+	res := &network.CapabilitiesResponse{Scope: network.LocalScope}
+	return res, nil
 }
 func (d *Driver) CreateNetwork(*network.CreateNetworkRequest) error {
-	fmt.Println("Hello")
+	// TODO create namespace
 	return nil
 }
 func (d *Driver) AllocateNetwork(*network.AllocateNetworkRequest) (*network.AllocateNetworkResponse, error) {
-	fmt.Println("Hello")
-	return nil, nil
+	res := &network.AllocateNetworkResponse{}
+	return res, nil
 }
 func (d *Driver) DeleteNetwork(*network.DeleteNetworkRequest) error {
-	fmt.Println("Hello")
 	return nil
 }
 func (d *Driver) FreeNetwork(*network.FreeNetworkRequest) error {
-	fmt.Println("Hello")
 	return nil
 }
 func (d *Driver) CreateEndpoint(*network.CreateEndpointRequest) (*network.CreateEndpointResponse, error) {
-	fmt.Println("Hello")
 	return nil, nil
 }
 func (d *Driver) DeleteEndpoint(*network.DeleteEndpointRequest) error {
-	fmt.Println("Hello")
 	return nil
 }
 func (d *Driver) EndpointInfo(*network.InfoRequest) (*network.InfoResponse, error) {
-	fmt.Println("Hello")
 	return nil, nil
 }
 func (d *Driver) Join(*network.JoinRequest) (*network.JoinResponse, error) {
-	fmt.Println("Hello")
 	return nil, nil
 }
 func (d *Driver) Leave(*network.LeaveRequest) error {
-	fmt.Println("Hello")
 	return nil
 }
 func (d *Driver) DiscoverNew(*network.DiscoveryNotification) error {
-	fmt.Println("Hello")
 	return nil
 }
 func (d *Driver) DiscoverDelete(*network.DiscoveryNotification) error {
-	fmt.Println("Hello")
 	return nil
 }
 func (d *Driver) ProgramExternalConnectivity(*network.ProgramExternalConnectivityRequest) error {
-	fmt.Println("Hello")
 	return nil
 }
 func (d *Driver) RevokeExternalConnectivity(*network.RevokeExternalConnectivityRequest) error {
-	fmt.Println("Hello")
 	return nil
 }
