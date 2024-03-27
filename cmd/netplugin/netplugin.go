c26076f26fd3e0f7d943b41b22f59c3079376d19
Lovro Grguric Mileusnic <lovrogm@gmail.com>
2024-03-27 12:07:28 +0100

    Add address and port option flags

--- cmd/netplugin/netplugin.go
+++ cmd/netplugin/netplugin.go
@@ -1,18 +1,26 @@
 package main
 
 import (
-	"dockercan/internal/driver"
+	"flag"
 	"log"
+
+	"dockercan/internal/driver"
 
 	"github.com/docker/go-plugins-helpers/network"
 )
 
 func main() {
+
+	ip := "127.0.0.1:4343"
+
+	flag.StringVar(&ip, "addr", ip, "IPv4 address with port")
+
+	flag.Parse()
 	d := driver.Driver{}
 	h := network.NewHandler(&d)
 
-	log.Println("Starting CAN docker network driver at 127.0.0.1:1337")
-	err := h.ServeTCP("lgm_dockercan", "127.0.0.1:1338", "", nil)
+	log.Printf("Starting docker CAN driver at %s\n", ip)
+	err := h.ServeTCP("dockercan", ip, "", nil)
 
 	if err != nil {
 		log.Panicln(err)
